e1cd341887822ebe560448f0970cf8746840436b
Ashley Chatman <ashchat2008@gmail.com>
2023-08-31 20:54:45 -0400

    Comments -Add js comments

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-const navToggler = document.querySelector(".navbar-toggler");
-const overlayMenu = document.querySelector(".overlay-menu");
+//Toggles the overlary menu on click
+const navToggler = document.querySelector(".navbar-toggler"); // class on button
+const overlayMenu = document.querySelector(".overlay-menu");  //div container class for unorderlist
 
 navToggler.onclick = function () {
   overlayMenu.classList.toggle("active");

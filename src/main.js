d9b1dd30bf493a15a8ff7a83b363e4c3dc86b1bf
Ashley Chatman <ashchat2008@gmail.com>
2023-09-04 16:03:36 -0400

    Overlay menu -Delete collapse class -Created overlay class -Add button -Style overlay menu

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-/*Toggles the overlary menu on click
+//Toggles the overlary menu on click
 const navToggler = document.querySelector(".navbar-toggler"); // class on button
 const overlayMenu = document.querySelector(".overlay-menu");  //div container class for unorderlist
+
+const navClose = document.querySelector(".closebtn");
 
 navToggler.onclick = function () {
   overlayMenu.classList.toggle("active");
 };
+navClose.onclick = function (){
+  overlayMenu.classList.toggle("close");
+}
 
-*/
